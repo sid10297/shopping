2286ae1fda5f02793b63aaea601d8aeb79e2ce57
Siddharth <siddharth10297@gmail.com>
2021-10-07 12:59:32 +0530

    Delete filter files and start filter from scratch

--- frontend/src/Components/ColorFilter.jsx
+++
@@ -1,9 +0,0 @@
-const ColorFilter = () => {
-  return (
-    <div>
-      <h1>Color filter</h1>
-    </div>
-  );
-};
-
-export default ColorFilter;
--- frontend/src/Components/PriceFilter.jsx
+++
@@ -1,9 +0,0 @@
-const PriceFilter = () => {
-  return (
-    <div>
-      <h1>Price Filter</h1>
-    </div>
-  );
-};
-
-export default PriceFilter;
