3ed08f6cdc2836b104cbf301d89d5defc394c54a
Siddharth <siddharth10297@gmail.com>
2021-10-25 12:12:19 +0530

    Get Quantity available updated and log in the console

--- backend/routes/placeOrder.js
+++ backend/routes/placeOrder.js
@@ -6,20 +6,42 @@ const Order = require("../models/placeOrder");
 const Product = require("../models/product");
 const { verifyPermission } = require("../verifyToken");
 
-router.post("/place-order", verifyPermission(BASIC), async (req, res) => {
-  const order = new Order({
-    cartItems: req.body.cartItems,
-    cartTotal: req.body.cartTotal,
-    userDetails: req.body.userDetails,
-  });
+router.post(
+  "/place-order",
+  verifyPermission(BASIC, ADMIN),
+  async (req, res) => {
+    const order = new Order({
+      cartItems: req.body.cartItems,
+      cartTotal: req.body.cartTotal,
+      userDetails: req.body.userDetails,
+    });
 
-  try {
-    const newOrder = await order.save();
-    res.json(newOrder);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
+    try {
+      const newOrder = await order.save();
+      const ids = newOrder.cartItems.map((_product) => _product.product._id);
+      const quantityToOrder = newOrder.cartItems.map(
+        (_product) => _product.quantityToOrder
+      );
+
+      const productQuantityToBeDecremented = await Product.find({ _id: ids });
+      const quantityAvailable = productQuantityToBeDecremented.map(
+        (n) => n.quantityAvailable
+      );
+      const decrementQuantity = quantityAvailable.map((item, index) => {
+        return item - quantityToOrder[index];
+      });
+
+      console.log(ids, "Id's");
+      console.log(quantityAvailable, "Available Quantity");
+      console.log(quantityToOrder, " quantityToOrder");
+      console.log(decrementQuantity, "Decremented Quantity");
+
+      res.json(newOrder);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   }
-});
+);
 
 router.get("/orders", verifyPermission(ADMIN), async (req, res) => {
   try {
