ed4234c474c16e3e4be1ac41e099f171ebd3a580
Siddharth <siddharth10297@gmail.com>
2021-10-26 16:19:27 +0530

    Fix footer margin

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,31 +1,30 @@
-import {
-  BottomNavigation,
-  BottomNavigationAction,
-  Typography,
-} from "@mui/material";
+import { BottomNavigation, Typography } from "@mui/material";
 
 const Footer = () => {
   return (
-    <BottomNavigation
-      showLabels
-      style={{
-        position: "fixed",
-        left: "0",
-        bottom: "0",
-        width: "100%",
-        textAlign: "center",
-        fontSize: "20px",
-        backgroundColor: "#2196f3",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        color: "white",
-      }}
-    >
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        &copy; Shoe Kart
-      </Typography>
-    </BottomNavigation>
+    <div style={{ marginTop: "100px" }}>
+      <BottomNavigation
+        showLabels
+        style={{
+          position: "fixed",
+          left: "0",
+          bottom: "0",
+          width: "100%",
+          textAlign: "center",
+          fontSize: "20px",
+          backgroundColor: "#2196f3",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+          padding: "20px",
+        }}
+      >
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          &copy; Shoe Kart
+        </Typography>
+      </BottomNavigation>
+    </div>
   );
 };
 
